50794bdc11bce11c7b3f3bbcb64c55d1ffd16bfb
SilverBullete <1256819071@qq.com>
2020-02-07 17:27:58 +0800

    init

--- TutorSelection/settings.py
+++ TutorSelection/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'TutorSelection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,5 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
