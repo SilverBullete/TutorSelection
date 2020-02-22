be6a2f7930aeaebe417fc453b1f278e24da70d60
SilverBullete <1256819071@qq.com>
2020-02-22 20:36:17 +0800

    commit 登录界面

--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Radio, RadioGroup, RadioButton } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
